a8f7d90254899cd8796c6d767c34bc846bfdb7af
heejun <zdidas@naver.com>
2018-09-10 20:36:31 +0900

    스크룰 추가, 서브페이지 추가  등등
    버그 등  수정  아직 더  수정해야함

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,4 @@
-   var swiper = new Swiper('.swiper-container', {
+   let swiper = new Swiper('.swiper-container', {
 
       spaceBetween: 30,
       centeredSlides: true,
@@ -23,20 +23,37 @@
   AOS.init();
 
 
+
+
+
+
+/*INDEX 스크롤  */
+
+    function fnMove(seq){   
+        let offset = $("#scroll-con0" + seq).offset();
+        
+        
+        $('html, body').animate({scrollTop : offset.top}, 1000);
+    }  
+
+
 /*fancybox */
   $('#ov-box a').fancybox();
 
 
-/* map 지도 */
-
-    function starbucksPosition(){
-      myMap=new naver.maps.Map(document.getElementById('mapA'),{
-        center:new naver.maps.LatLng(37.4851459, 127.11931619999996),
-        zoom:12
-      });
-      new naver.maps.Marker({
-        position:new naver.maps.LatLng(37.4851459,127.11931619999996),
-        map:myMap
-      });
-    }
-    window.onload=starbucksPosition;
+/* map 지도  회사소개*/  
+//        function starbucksPosition() {
+//            myMap = new naver.maps.Map(document.getElementById('map-box'), {
+//                center: new naver.maps.LatLng(37.4851459, 127.11931619999996),
+//                zoom: 12
+//            });
+//            new naver.maps.Marker({
+//                position: new naver.maps.LatLng(37.4851459, 127.11931619999996),
+//                map: myMap
+//            });
+//        }
+//        window.onload = starbucksPosition;
+//        
+//        
+//        
+//        $('#ov-box a').fancybox();
